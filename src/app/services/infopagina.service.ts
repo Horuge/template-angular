156504803a5f6e1ddd40708951dc55469c76ab51
horuge <jorge.solis.es@gmail.com>
2018-08-19 18:13:19 +0200

    Fin seccion 6

--- src/app/services/infopagina.service.ts
+++ src/app/services/infopagina.service.ts
@@ -9,15 +9,26 @@ export class InfopaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {
         this.cargada = true;
         this.info = resp;
-        console.log(resp);
       });
   }
 
-
+  private cargarEquipo() {
+    this.http.get('https://angular-html-c7ca9.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+        this.equipo = resp;
+        console.log(this.equipo);
+      });
+  }
 }
